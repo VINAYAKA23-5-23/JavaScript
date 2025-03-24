4ddbdc98343adc02844107a55e3d9d6b320a6670
VINAYAKA23-5-23 <vinayakaangadi96@gmail.com>
2025-03-24 01:35:50 +0000

    conversion of datatypes

---
+++ Basics/ConversionOperation.js
@@ -0,0 +1,27 @@
+let score="33"
+
+
+console.log(typeof score);//String
+let valInNumber=Number(score)
+console.log(typeof valInNumber);
+consol.log(valInNumber);
+
+//"33"=>33
+//"33abc"=>NaN
+//true=>1
+//false=>0
+//null=>0
+//undifined=>NaN
+
+let isLoggedIn=1;
+
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+consol,log(booleanIsLoggedIn)//true
+
+//""=>false
+//"abdd"=>true
+//1=>true; 0=>false
+
+let val=33
+let stringval=String(val)
+consol.log(stringval)//33 but the type is String
